b61f2145c91bd93af1ab37eb00d0fe5a386a90a0
peanut-lover <jihoson94@gmail.com>
2022-12-14 17:48:59 +0900

    :sparkles: init

---
+++ src/navigation/RootNavigator.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ActivityIndicator} from 'react-native';
+
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+const linking = {
+  prefixes: ['peoplesapp://'],
+};
+
+const RootStack = createNativeStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer
+      linking={linking}
+      fallback={<ActivityIndicator color="blue" size="large" />}>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Details" component={DetailsScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
